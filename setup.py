f569742cf89ce8f17b1bcf032334a4a1801f78bf
Jack Adamson <jack@mrfluffybunny.com>
2019-12-14 16:37:11 +1100

    Added CI/CD

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 import setuptools
+import re
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("facebooklogger/_version.py", "r") as f:
+    version_match = re.search(
+        r'^version \s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE
+    )
+    if version_match is None:
+        raise ValueError("Version not found in facebooklogger/_version.py")
+    version = version_match.group(1)
+
 setuptools.setup(
     name="facebooklogger",
-    version="0.1.0",
+    version=version,
     author="Jack Adamson",
     author_email="jack@mrfluffybunny.com",
     description="A logging handler that sends you log entries on Facebook Messenger",
@@ -14,6 +23,14 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jackadamson/facebooklogger",
     packages=setuptools.find_packages(),
-    classifiers=["Programming Language :: Python :: 3"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: System :: Logging",
+        "Topic :: System :: Monitoring",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Development Status :: 4 - Beta",
+    ],
     python_requires=">=3.6",
 )
